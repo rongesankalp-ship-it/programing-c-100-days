599605577efe249ca8077e219f1da86747278a1a
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 20:52:22 +0530

    Implement LCM calculation program in C

---
+++ 38.c
@@ -0,0 +1,25 @@
+/*Write a program to find the sum of digits of a number.*/
+
+#include <stdio.h>
+
+void main(){
+    int a, b, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if(a > b){
+        lcm = a;
+    }
+    else{
+        lcm = b;
+    }
+
+    while(1){
+        if(lcm % a == 0 && lcm % b == 0){
+            printf("LCM of %d and %d = %d\n", a, b, lcm);
+            break;
+        }
+        lcm++;
+    }
+}
