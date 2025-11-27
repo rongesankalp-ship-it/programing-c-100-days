43cedf3a711878f10172a99efca0cc169a1fe02f
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 19:49:53 +0530

    Add program to calculate sum of first n odd numbers

---
+++ 27.c
@@ -0,0 +1,16 @@
+/*Write a program to print the sum of the first n odd numbers.*/
+
+#include <stdio.h>
+
+void main(){
+    int n, i, sum = 0;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++){
+        sum = sum + (2 * i - 1);
+    }
+
+    printf("Sum of first %d odd numbers = %d\n", n, sum);
+}
