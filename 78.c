f3c1936428a5e529147d564a187cf25c2532dba3
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 23:06:43 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,19 @@
+// Sum of main diagonal elements
+
+#include <stdio.h>
+
+void main(){
+    int n, i, j, a[10][10], sum = 0;
+
+    scanf("%d %d", &n, &n);
+
+    for(i = 0; i < n; i++){
+        for(j = 0; j < n; j++){
+            scanf("%d", &a[i][j]);
+            if(i == j)
+                sum = sum + a[i][j];
+        }
+    }
+
+    printf("%d\n", sum);
+}
