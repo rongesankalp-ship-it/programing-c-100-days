b6157b36b8e8cb7c2e55a8d4739b0debef539ecd
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 20:49:39 +0530

    Add program to find HCF (GCD) of two numbers

---
+++ 36.c
@@ -0,0 +1,18 @@
+/*Write a program to find the HCF (GCD) of two numbers.*/
+
+#include <stdio.h>
+
+void main(){
+    int a, b, i, hcf;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    for(i = 1; i <= a && i <= b; i++){
+        if(a % i == 0 && b % i == 0){
+            hcf = i;
+        }
+    }
+
+    printf("HCF of %d and %d = %d\n", a, b, hcf);
+}
