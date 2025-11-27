7a185ad9c77e0180e3bca4703bb4337799ec5fbf
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 22:35:14 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,22 @@
+/*Read and print elements of a one-dimensional array.*/
+
+#include <stdio.h>
+
+void main(){
+    int n, i, arr[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+    for(i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+}
