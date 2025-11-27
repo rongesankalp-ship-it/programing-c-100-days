7c7ca59fd470de3a17864459173fb947b48c5cef
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 23:21:39 +0530

    Create 91.c

---
+++ 91.c
@@ -0,0 +1,18 @@
+// Remove all vowels
+
+#include <stdio.h>
+
+void main(){
+    char s[100];
+    int i;
+
+    gets(s);
+
+    for(i = 0; s[i] != '\0'; i++){
+        if(!(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
+             s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'))
+            printf("%c", s[i]);
+    }
+
+    printf("\n");
+}
