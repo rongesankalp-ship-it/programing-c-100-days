aae6f2e4969801fb2c0046ae23ec47e349c32bed
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 22:37:04 +0530

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,30 @@
+/*Find the maximum and minimum element in an array.*/
+
+#include <stdio.h>
+
+void main(){
+    int n, i, arr[100], max, min;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    max = arr[0];
+    min = arr[0];
+
+    for(i = 1; i < n; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+}
