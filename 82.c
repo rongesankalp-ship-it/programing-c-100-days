887195cb95fe0b526265481891cea13e3437e486
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 23:10:28 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,13 @@
+// Print each character on a new line
+
+#include <stdio.h>
+
+void main(){
+    char s[100];
+    int i;
+
+    gets(s);
+
+    for(i = 0; s[i] != '\0'; i++)
+        printf("%c\n", s[i]);
+}
