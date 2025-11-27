049fa7b3bb56ea03708e36995bcf32936a99684f
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 22:59:26 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,28 @@
+// Find the sum of each row of a matrix and store it in an array
+
+#include <stdio.h>
+
+void main(){
+    int r, c, i, j, a[10][10], sum[10];
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++){
+        for(j = 0; j < c; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++){
+        sum[i] = 0;
+        for(j = 0; j < c; j++){
+            sum[i] = sum[i] + a[i][j];
+        }
+    }
+
+    for(i = 0; i < r; i++){
+        printf("%d ", sum[i]);
+    }
+
+    printf("\n");
+}
