2e6591a37c2b3c96305f30cc69bac05dd88fbb57
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 23:12:49 +0530

    Create 84.c

---
+++ 84.c
@@ -0,0 +1,17 @@
+// Convert lowercase to uppercase
+
+#include <stdio.h>
+
+void main(){
+    char s[100];
+    int i;
+
+    gets(s);
+
+    for(i = 0; s[i] != '\0'; i++){
+        if(s[i] >= 'a' && s[i] <= 'z')
+            s[i] = s[i] - 32;
+    }
+
+    printf("%s\n", s);
+}
