47625cc1b56e97834e742332ba27e82dba4cb546
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 23:28:16 +0530

    Create 97.c

---
+++ 97.c
@@ -0,0 +1,19 @@
+// Print initials of a name
+
+#include <stdio.h>
+
+void main(){
+    char s[100];
+    int i;
+
+    gets(s);
+
+    printf("%c.", s[0]);
+
+    for(i = 0; s[i] != '\0'; i++){
+        if(s[i] == ' ')
+            printf("%c.", s[i + 1]);
+    }
+
+    printf("\n");
+}
