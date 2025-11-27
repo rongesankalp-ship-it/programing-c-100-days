071dca7c21658c0770a79b0e87f34957e34a0f40
rongesankalp-ship-it <rongesankalp@gmail.com>
2025-11-27 23:17:47 +0530

    Create 88.c

---
+++ 88.c
@@ -0,0 +1,17 @@
+// Replace spaces with hyphens
+
+#include <stdio.h>
+
+void main(){
+    char s[100];
+    int i;
+
+    gets(s);
+
+    for(i = 0; s[i] != '\0'; i++){
+        if(s[i] == ' ')
+            s[i] = '-';
+    }
+
+    printf("%s\n", s);
+}
